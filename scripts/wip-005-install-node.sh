51c4201ce634a45d382e1f9c04779771e164ef2e
Bertus Kruger <krugercreations@gmail.com>
2016-01-02 01:19:18 +0100

    Update wip-005-install-node.sh

--- scripts/wip-005-install-node.sh
+++ scripts/wip-005-install-node.sh
@@ -1,3 +1,9 @@
 sudo apt-get -y remove nodejs
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb
-sudo dpkg -i node_latest_armhf.deb
+
+# Latest Version
+#wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+#sudo dpkg -i node_latest_armhf.deb
+
+wget https://nodejs.org/download/release/v0.12.1/
+sudo apt-get install node
+# needs a restart
