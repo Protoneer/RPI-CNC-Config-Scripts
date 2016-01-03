729a536d4e5f2f5552133603435c12c272064a80
Protoneer <info@protoneer.co.nz>
2016-01-03 07:11:00 +0000

    LaserWeb

--- scripts/wip-006-install-laserweb.sh
+++ scripts/wip-006-install-laserweb.sh
@@ -2,5 +2,5 @@ sudo apt-get -y install build-essential python g++ make
 cd ~/Desktop
 sudo git clone https://github.com/openhardwarecoza/LaserWeb.git
 cd LaserWeb
-sudo npm install serialport@1.6.1
+#sudo npm install serialport@1.6.1
 sudo npm install socket.io node-static
