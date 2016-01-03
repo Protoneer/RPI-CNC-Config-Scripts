7aea2460ff620a703ebdef0125208005f389146b
Bertus Kruger <krugercreations@gmail.com>
2016-01-03 21:09:50 +0100

    Update wip-006-install-laserweb.sh

--- scripts/wip-006-install-laserweb.sh
+++ scripts/wip-006-install-laserweb.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y install build-essential python g++ make
-cd ~/Desktop
+cd ~
 sudo git clone https://github.com/openhardwarecoza/LaserWeb.git
 cd LaserWeb
 npm install -g serialport@1.6.1
-npm install -g socket.io node-static dotenv
+npm install -g socket.io node-static
