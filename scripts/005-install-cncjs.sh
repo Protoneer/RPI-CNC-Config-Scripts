c7b2d5ff889978daa4e25e6ebc3956c72a72e647
Bertus Kruger <krugercreations@gmail.com>
2016-01-04 06:55:27 +0100

    Update 005-install-cncjs.sh

--- scripts/005-install-cncjs.sh
+++ scripts/005-install-cncjs.sh
@@ -1,6 +1,6 @@
-npm install -g socket.io
-npm install -g socket.io-parser
 echo "prefix=~/.npm" >> ~/.npmrc
 cat ~/.npmrc
+npm install -g socket.io
+npm install -g socket.io-parser
 npm install -g cncjs
 export PATH=$PATH:~/.npm/bin/
