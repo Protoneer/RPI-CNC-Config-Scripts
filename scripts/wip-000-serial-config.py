cc1848f73c6dc4e2c88e09d6c67b644f75956e5c
Bertus Kruger <krugercreations@gmail.com>
2016-03-21 12:41:39 +1300

    Update wip-000-serial-config.py

--- scripts/wip-000-serial-config.py
+++ scripts/wip-000-serial-config.py
@@ -1,3 +1,12 @@
 # Check Pi version 
+#sudo python
+#import RPi.GPIO as GPIO
+#GPIO.RPI_REVISION
 
 # If V3 change Bluetooth settings
+
+  # 
+  #sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+
+  # Stop BT service
+  #sudo systemctl disable hciuart
