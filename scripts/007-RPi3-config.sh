0b1f9bcc83e221ca7a0e72f46675f7c92e5f950d
Bertus Kruger <krugercreations@gmail.com>
2016-03-21 14:18:26 +1300

    Update and rename scripts/wip/000-RPi3-config.sh to scripts/007-RPi3-config.sh

---
+++ scripts/007-RPi3-config.sh
@@ -0,0 +1,2 @@
+sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+sudo systemctl disable hciuart
--- scripts/wip/000-RPi3-config.sh
+++
@@ -1,2 +0,0 @@
-echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
-systemctl disable hciuart
