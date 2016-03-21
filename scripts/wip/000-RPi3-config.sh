3202a3237bcee6c9a917e510e47d776e7c422733
Bertus Kruger <krugercreations@gmail.com>
2016-03-21 14:12:45 +1300

    Update 000-RPi3-config.sh

--- scripts/wip/000-RPi3-config.sh
+++ scripts/wip/000-RPi3-config.sh
@@ -1,4 +1,2 @@
-sudo su
-sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
-exit
-sudo systemctl disable hciuart
+echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+systemctl disable hciuart
