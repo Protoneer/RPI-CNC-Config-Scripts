3d66e15fd9a330ad0c811965291af61d7e912bce
Bertus Kruger <krugercreations@gmail.com>
2016-03-21 13:29:04 +1300

    Update 005-install-cncjs.sh

--- scripts/005-install-cncjs.sh
+++ scripts/005-install-cncjs.sh
@@ -1,5 +1,9 @@
 echo "prefix=~/.npm" >> ~/.npmrc
 cat ~/.npmrc
+npm config set user 0
+npm config set unsafe-perm true
+
+
 npm install -g socket.io
 npm install -g socket.io-parser
 npm install -g cncjs
