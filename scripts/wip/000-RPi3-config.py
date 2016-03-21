2f450a84c36961271e1fa7b0d7255888fd7ea39e
Bertus Kruger <krugercreations@gmail.com>
2016-03-21 14:07:07 +1300

    Update 000-RPi3-config.py

--- scripts/wip/000-RPi3-config.py
+++ scripts/wip/000-RPi3-config.py
@@ -1,12 +1,4 @@
-# Check Pi version 
-#sudo python
-#import RPi.GPIO as GPIO
-#GPIO.RPI_REVISION
-
-# If V3 change Bluetooth settings
-
-  # 
-  #sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
-
-  # Stop BT service
-  #sudo systemctl disable hciuart
+sudo su
+sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+exit
+sudo systemctl disable hciuart
