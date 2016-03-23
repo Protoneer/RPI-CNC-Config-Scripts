95e28a73db49942616426488f2371d0040518bf2
Bertus Kruger <krugercreations@gmail.com>
2016-03-23 17:20:37 +1300

    Update 001-avrdude.sh

--- scripts/wip/001-avrdude.sh
+++ scripts/wip/001-avrdude.sh
@@ -3,5 +3,5 @@ sudo apt-get install avrdude -y
 
 ## https://github.com/CisecoPlc/avrdude-rpi
 wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9i_atmega328p_16mhz_115200.hex
-avrdude -p m328p -P /dev/ttyAMA0 -b 57600 -c arduino -vvvv -n
-grbl_v0_9i_atmega328p_16mhz_115200.hex
+
+sudo avrdude -c arduino -P /dev/ttyAMA0 -b 57600 -v -p atmega328p -D -U flash:w:grbl_v0_9i_atmega328p_16mhz_115200.hex
