a11c8ab7c21f7fb781f0d33269555bd0bf3537d1
Bertus Kruger <krugercreations@gmail.com>
2016-03-23 17:25:27 +1300

    Update 001-avrdude.sh

--- scripts/wip/001-avrdude.sh
+++ scripts/wip/001-avrdude.sh
@@ -5,3 +5,5 @@ sudo apt-get install avrdude -y
 wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9i_atmega328p_16mhz_115200.hex
 
 sudo avrdude -c arduino -P /dev/ttyAMA0 -b 57600 -v -p atmega328p -D -U flash:w:grbl_v0_9i_atmega328p_16mhz_115200.hex
+
+-C/home/bertus/Tools/arduino-1.6.4/hardware/tools/avr/etc/avrdude.conf
