3a07ef9baf42bf24f7dd981cb235d103a45973d7
Bertus Kruger <krugercreations@gmail.com>
2016-04-03 08:33:02 +1200

    Update 001-avrdude.sh

--- scripts/wip/001-avrdude.sh
+++ scripts/wip/001-avrdude.sh
@@ -7,3 +7,10 @@ wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9i
 sudo avrdude -c arduino -P /dev/ttyAMA0 -b 57600 -v -p atmega328p -Cavrdude.conf -D -U flash:w:grbl_v0_9i_atmega328p_16mhz_115200.hex
 
 -Cavrdude.conf
+
+
+Good info!!!
+https://github.com/openenergymonitor/avrdude-rpi
+
+To upload to the emonPi (ATmega328 @ 16Mhz):
+$ avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:sketch_name.hex
