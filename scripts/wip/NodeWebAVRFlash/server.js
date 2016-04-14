926f123ed7455132c89977d6211ed506d22fea8b
Bertus Kruger <krugercreations@gmail.com>
2016-04-14 21:10:55 +1200

    Update server.js

--- scripts/wip/NodeWebAVRFlash/server.js
+++ scripts/wip/NodeWebAVRFlash/server.js
@@ -1,4 +1,4 @@
 var exec = require('child_process').execSync;
 var download = exec('wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9j_atmega328p_16mhz_115200.hex -O firmware.hex')
-var html = exec('dir').toString()
+var html = exec('avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:firmware.hex').toString()
 console.log(html);
