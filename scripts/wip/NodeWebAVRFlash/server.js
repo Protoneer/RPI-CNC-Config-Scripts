f15039e353747f9a5c5d6c8d61a12f949930bd9e
Bertus Kruger <krugercreations@gmail.com>
2016-04-14 20:57:43 +1200

    Update server.js

--- scripts/wip/NodeWebAVRFlash/server.js
+++ scripts/wip/NodeWebAVRFlash/server.js
@@ -1,3 +1,4 @@
 var exec = require('child_process').execSync;
+var download = exec('wget',['https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9j_atmega328p_16mhz_115200.hex'])
 var html = exec('dir').toString()
 console.log(html);
