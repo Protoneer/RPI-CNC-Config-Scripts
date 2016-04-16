cc1f55b8df2f888c4fc5d9d3d4979c6a0f5b6a90
Bertus Kruger <krugercreations@gmail.com>
2016-04-16 18:42:15 +1200

    Update server.js

--- scripts/wip/WebAVRFlasher/lib/server.js
+++ scripts/wip/WebAVRFlasher/lib/server.js
@@ -28,7 +28,7 @@ server.post('/flash', function (req, res) {
 	var exec = require('child_process').execSync;
 	var download = exec('wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9j_atmega328p_16mhz_115200.hex -O firmware.hex')
 	//var flashResults = exec('avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:firmware.hex').toString();
-	var flashResults = exec('sh flash.sh').toString();
+	var flashResults = exec('sh lib/flash.sh').toString();
 	console.log(flashResults.indexOf("flash verified") > -1);
 	console.log(flashResults.length);
 	console.log(flashResults);
