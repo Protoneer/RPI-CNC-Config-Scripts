b339e41fcba2223c30998bfb01cc6fc55b08aa2e
Bertus Kruger <krugercreations@gmail.com>
2016-04-17 19:46:23 +1200

    Cleaned up

--- scripts/wip/WebAVRFlasher/lib/server.js
+++ scripts/wip/WebAVRFlasher/lib/server.js
@@ -44,7 +44,7 @@ server.post('/flash', function (req, res) {
 
 	var exec = require('child_process').execSync;
 	//var download = exec('wget https://raw.githubusercontent.com/grbl/grbl-builds/master/builds/grbl_v0_9j_atmega328p_16mhz_115200.hex -O firmware.hex')
-	var flashResults = exec('avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:/lib/uploads/firmware.hex').toString();
+	var flashResults = exec('avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:./lib/uploads/firmware.hex').toString();
 	console.log(flashResults);
 	res.json(results);		
 });
