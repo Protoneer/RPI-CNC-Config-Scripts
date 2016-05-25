4064a8addf0b262ffe692361442cb3dbed3e7d9e
Bertus Kruger <krugercreations@gmail.com>
2016-05-25 20:53:08 +1200

    Update install.sh

--- scripts/wip/WebAVRFlasher/avrdude-rpi/install.sh
+++ scripts/wip/WebAVRFlasher/avrdude-rpi/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y avrdude python-dev python-rpi.gpio minicom
-cp autoreset /usr/bin
-cp avrdude-autoreset /usr/bin
-mv /usr/bin/avrdude /usr/bin/avrdude-original
-ln -s /usr/bin/avrdude-autoreset /usr/bin/avrdude
+sudo cp autoreset /usr/bin
+sudo cp avrdude-autoreset /usr/bin
+sudo mv /usr/bin/avrdude /usr/bin/avrdude-original
+sudo ln -s /usr/bin/avrdude-autoreset /usr/bin/avrdude
