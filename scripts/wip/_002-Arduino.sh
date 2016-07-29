c3ee89eeb6b0c4e803d3e6b4e4f99bf82a52938b
Bertus Kruger <krugercreations@gmail.com>
2016-07-30 08:07:37 +1200

    Update _002-Arduino.sh

--- scripts/wip/_002-Arduino.sh
+++ scripts/wip/_002-Arduino.sh
@@ -5,3 +5,6 @@ sudo apt-get install arduino -y
 # This bit is not working yet.
 cat 'KERNEL=="ttyAMA0",SYMLINK+="ttyS0" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
 cat 'KERNEL=="ttyACM0",SYMLINK+="ttyS1" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
+
+
+# This link will help with setting up above : http://spellfoundry.com/sleepy-pi/setting-arduino-ide-raspbian/#Step_3_Link_the_Serial_port_to_the_Arduino_IDE
