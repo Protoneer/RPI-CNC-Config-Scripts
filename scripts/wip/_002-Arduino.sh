3d7181c01e0b9a298643e7d376182e08c76f128a
Bertus Kruger <krugercreations@gmail.com>
2016-07-30 19:44:07 +1200

    Update _002-Arduino.sh

--- scripts/wip/_002-Arduino.sh
+++ scripts/wip/_002-Arduino.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # install Aruino
-https://downloads.arduino.cc/arduino-1.6.10-linuxarm.tar.xz
+cd  ~
+wget https://downloads.arduino.cc/arduino-1.6.10-linuxarm.tar.xz
 tar xf arduino-1.6.10-linuxarm.tar.xz
 rm arduino-1.6.10-linuxarm.tar.xz
 
-cd ~
+
 cd arduino-1.6.10/hardware/tools/avr/bin
 sudo mv arduino-1.6.10/hardware/tools/avr/bin/avrdude arduino-1.6.10/hardware/tools/avr/bin/avrdude-original
+# Need local copies and time to 15ms
 wget https://raw.githubusercontent.com/Protoneer/avrdude-rpi/master/autoreset
 wget https://raw.githubusercontent.com/Protoneer/avrdude-rpi/master/avrdude-autoreset
 sudo ln -s avrdude-autoreset avrdude
-sudo chmode 777 avrdude
+sudo chmod 777 avrdude
 
 
 # This bit is not working yet.
-cat 'KERNEL=="ttyAMA0",SYMLINK+="ttyS0" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
-cat 'KERNEL=="ttyACM0",SYMLINK+="ttyS1" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
+#cat 'KERNEL=="ttyAMA0",SYMLINK+="ttyS0" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
+#cat 'KERNEL=="ttyACM0",SYMLINK+="ttyS1" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
 
 
 # This link will help with setting up above : http://spellfoundry.com/sleepy-pi/setting-arduino-ide-raspbian/#Step_3_Link_the_Serial_port_to_the_Arduino_IDE
