a01fac913e9c056d480f2a62eb101fc1a608c9af
Bertus Kruger <krugercreations@gmail.com>
2016-07-30 15:59:02 +1200

    Update _002-Arduino.sh

--- scripts/wip/_002-Arduino.sh
+++ scripts/wip/_002-Arduino.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install arduino -y
+# install Aruino
+https://downloads.arduino.cc/arduino-1.6.10-linuxarm.tar.xz
+tar xf arduino-1.6.10-linuxarm.tar.xz
+rm arduino-1.6.10-linuxarm.tar.xz
+
+
 
 # This bit is not working yet.
 cat 'KERNEL=="ttyAMA0",SYMLINK+="ttyS0" GROUP="dialout"'  >> '/etc/udev/rules.d/99-tty.rules'
@@ -8,3 +13,6 @@ cat 'KERNEL=="ttyACM0",SYMLINK+="ttyS1" GROUP="dialout"'  >> '/etc/udev/rules.d/
 
 
 # This link will help with setting up above : http://spellfoundry.com/sleepy-pi/setting-arduino-ide-raspbian/#Step_3_Link_the_Serial_port_to_the_Arduino_IDE
+
+
+
