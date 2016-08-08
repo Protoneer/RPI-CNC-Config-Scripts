0fe2a705a708740bb4622ce27344548daaa069dd
Bertus Kruger <krugercreations@gmail.com>
2016-08-08 18:31:08 +1200

    Update 008-Arduino.sh

--- scripts/008-Arduino.sh
+++ scripts/008-Arduino.sh
@@ -8,9 +8,9 @@ tar xf ~/arduino-1.6.10-linuxarm.tar.xz
 rm ~/arduino-1.6.10-linuxarm.tar.xz
 
 # Install avrdude-rpi
-cp autoreset2560 ~/arduino-1.6.10/hardware/tools/avr/bin/
-cp autoreset328 ~/arduino-1.6.10/hardware/tools/avr/bin/
-cp avrdude-autoreset ~/arduino-1.6.10/hardware/tools/avr/bin/
+cp avrdude-rpi/autoreset2560 ~/arduino-1.6.10/hardware/tools/avr/bin/
+cp avrdude-rpi/autoreset328 ~/arduino-1.6.10/hardware/tools/avr/bin/
+cp avrdude-rpi/avrdude-autoreset ~/arduino-1.6.10/hardware/tools/avr/bin/
 sudo mv ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude-original
 sudo ln -s ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude-autoreset ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude
 
