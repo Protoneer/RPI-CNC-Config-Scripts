2058eafbc3e1ac0c1c310143ea5e5e692346a6fd
Bertus Kruger <krugercreations@gmail.com>
2016-08-09 20:39:20 +1200

    Update 004-install-node.sh

--- scripts/004-install-node.sh
+++ scripts/004-install-node.sh
@@ -1,10 +1,18 @@
 sudo apt-get -y remove nodejs
 
 # RPi 3
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs
 
-# RPI all
+
+#RPI 3
+wget https://nodejs.org/dist/v4.4.7/node-v4.4.7-linux-armv7l.tar.xz
+tar -xvf node-v4.4.7-linux-armv7l.tar.gz
+cd node-v4.4.7-linux-armv7l
+sudo cp -R * /usr/local/
+
+
+# RPI 1+2
 wget https://nodejs.org/dist/v4.4.7/node-v4.4.7-linux-armv6l.tar.gz
 tar -xvf node-v4.4.7-linux-armv6l.tar.gz
 cd node-v4.4.7-linux-armv6l
