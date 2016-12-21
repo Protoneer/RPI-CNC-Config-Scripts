c593fc5653b5436290496953574c7f2dd67f8c9f
Bertus Kruger <krugercreations@gmail.com>
2016-12-21 19:31:53 +1300

    Added UGS installer

---
+++ scripts/wip/UGS.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+
+# install java
+sudo apt-get install openjdk-7-jre -y
+sudo apt-get install librxtx-java -y
+
+mkdir ugs
+cd ugs 
+
+# download UGS 1.09
+wget http://bit.ly/1M6z2ys -O ugs.zip
+
+unzip ugs.zip
+jar -xf UniversalGcodeSender.jar
+
+chmod +x start.sh
