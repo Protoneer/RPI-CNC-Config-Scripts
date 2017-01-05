0cc032e18d24b8967720351b189e23db4afbdc16
Bertus Kruger <krugercreations@gmail.com>
2017-01-05 23:16:31 +1300

    Update 00-RemoveAndClean.sh

--- scripts/00-RemoveAndClean.sh
+++ scripts/00-RemoveAndClean.sh
@@ -5,4 +5,7 @@ sudo apt-get remove --purge wolfram-engine
 # Clean out cache
 sudo apt-get clean
 
-#rm ~/RPI-CNC-Config-Scripts
+# Update
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
