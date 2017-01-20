b81afae0cc952458a1db4034adaa6059207ced70
Protoneer <info@protoneer.co.nz>
2017-01-20 07:43:16 +0000

    Fixed firmware upload scripts

--- scripts/008-GRBL-V0.9J.sh
+++ scripts/008-GRBL-V0.9J.sh
@@ -1,1 +1,1 @@
-avrdude -v -C ~/arduino-1.8.1/hardware/tools/avr/etc/avrdude.conf -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:../resources/firmware/grbl_v0_9j_atmega328p_16mhz_115200.hex
+/home/pi/arduino-1.8.1/hardware/tools/avr/bin/avrdude -C/home/pi/arduino-1.8.1/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyAMA0 -b115200 -D -Uflash:w:/home/pi/RPI-CNC-Config-Scripts/resources/firmware/grbl_v0_9j_atmega328p_16mhz_115200.hex:i 
