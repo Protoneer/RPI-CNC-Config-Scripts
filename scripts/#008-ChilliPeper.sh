4ae92eb660c630b8d7f8b34d6c1e5b64f6e30b15
Bertus Kruger <krugercreations@gmail.com>
2017-01-20 21:41:19 +1300

    Update #008-ChilliPeper.sh

--- scripts/#008-ChilliPeper.sh
+++ scripts/#008-ChilliPeper.sh
@@ -1,1 +1,3 @@
+cd ~
 wget https://github.com/chilipeppr/serial-port-json-server/releases/download/v1.88/serial-port-json-server-1.88_linux_arm.tar.gz
+tar -xvzf serial-port-json-server-1.88_linux_arm.tar.gz
