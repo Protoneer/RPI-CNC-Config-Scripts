581f29577198b7ed14ea4f39e61914984899b36a
Bertus Kruger <krugercreations@gmail.com>
2017-01-20 15:37:08 +1300

    Update 008-GRBL-V0.9J.sh

--- scripts/008-GRBL-V0.9J.sh
+++ scripts/008-GRBL-V0.9J.sh
@@ -1,1 +1,1 @@
-avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:~/RPI-CNC-Config-Scripts/resources/firmware/grbl_v0_9j_atmega328p_16mhz_115200.hex
+avrdude -v -C ~/arduino-1.8.1/hardware/tools/avr/etc/avrdude.conf -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 115200 -U flash:w:~/RPI-CNC-Config-Scripts/resources/firmware/grbl_v0_9j_atmega328p_16mhz_115200.hex
