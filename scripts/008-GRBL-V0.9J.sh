cb4b2bfad7feec90a50a6c59b2e1ff6a6f420be6
Bertus Kruger <krugercreations@gmail.com>
2017-01-22 08:43:28 +1300

    Rename scripts/008-GRBL-V0.9J.sh to resources/scripts/Run-Flash-GRBL-V0.9J.sh

--- scripts/008-GRBL-V0.9J.sh
+++
@@ -1,1 +0,0 @@
-/home/pi/arduino-1.8.1/hardware/tools/avr/bin/avrdude -C/home/pi/arduino-1.8.1/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyAMA0 -b115200 -D -Uflash:w:/home/pi/RPI-CNC-Config-Scripts/resources/firmware/grbl_v0_9j_atmega328p_16mhz_115200.hex:i 
