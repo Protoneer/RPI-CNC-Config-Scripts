de213340470aa2b21f0ec3c58d493ff67c7f5ec5
Bertus Kruger <krugercreations@gmail.com>
2017-01-21 19:08:51 +1300

    Update 000-UpdateAndClean.sh

--- scripts/000-UpdateAndClean.sh
+++ scripts/000-UpdateAndClean.sh
@@ -4,8 +4,8 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
 
-sudo apt-get remove --purge libreoffice-*
-sudo apt-get remove --purge wolfram-engine
+sudo apt-get -y remove --purge libreoffice-*
+sudo apt-get -y remove --purge wolfram-engine
 
 
 # Clean out cache
