fb9f5f8714e16243c6be638e0680ac9539139cdd
Bertus Kruger <krugercreations@gmail.com>
2017-12-12 14:33:36 +1300

    Update 007-install-Arduino.sh

--- scripts/007-install-Arduino.sh
+++ scripts/007-install-Arduino.sh
@@ -3,23 +3,23 @@
 sudo apt-get install -y python-dev python-rpi.gpio
 
 # install Aruino
-wget https://downloads.arduino.cc/arduino-1.8.1-linuxarm.tar.xz -P ~
-tar xf ~/arduino-1.8.1-linuxarm.tar.xz -C ~
-rm ~/arduino-1.8.1-linuxarm.tar.xz
+wget https://downloads.arduino.cc/arduino-1.8.5-linuxarm.tar.xz -P ~
+tar xf ~/arduino-1.8.5-linuxarm.tar.xz -C ~
+rm ~/arduino-1.8.5-linuxarm.tar.xz
 
 # Install avrdude-rpi
-cp avrdude-rpi/autoreset2560 ~/arduino-1.8.1/hardware/tools/avr/bin/
-cp avrdude-rpi/autoreset328 ~/arduino-1.8.1/hardware/tools/avr/bin/
-cp avrdude-rpi/avrdude-autoreset ~/arduino-1.8.1/hardware/tools/avr/bin/
-sudo mv ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude-original
-sudo ln -s ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude-autoreset ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude
+cp avrdude-rpi/autoreset2560 ~/arduino-1.8.5/hardware/tools/avr/bin/
+cp avrdude-rpi/autoreset328 ~/arduino-1.8.5/hardware/tools/avr/bin/
+cp avrdude-rpi/avrdude-autoreset ~/arduino-1.8.5/hardware/tools/avr/bin/
+sudo mv ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude-original
+sudo ln -s ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude-autoreset ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude
 
 
-sudo chmod +x ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude
-sudo chmod +x ~/arduino-1.8.1/hardware/tools/avr/bin/autoreset328
-sudo chmod +x ~/arduino-1.8.1/hardware/tools/avr/bin/autoreset2560
+sudo chmod +x ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude
+sudo chmod +x ~/arduino-1.8.5/hardware/tools/avr/bin/autoreset328
+sudo chmod +x ~/arduino-1.8.5/hardware/tools/avr/bin/autoreset2560
 
 # Make AVRDUDE available for all
-sudo ln -s -T ~/arduino-1.8.1/hardware/tools/avr/bin/avrdude /usr/local/bin/avrdude
+sudo ln -s -T ~/arduino-1.8.5/hardware/tools/avr/bin/avrdude /usr/local/bin/avrdude
 #  export PATH=$PATH:~/arduino-1.8.1/hardware/tools/avr/bin
 
