9e0bf2040462f9cfffcbb3c6dd1f34602a923bf4
Bertus Kruger <krugercreations@gmail.com>
2017-12-12 13:56:40 +1300

    Update 000-UpdateCleanGUI.sh

--- scripts/000-UpdateCleanGUI.sh
+++ scripts/000-UpdateCleanGUI.sh
@@ -2,14 +2,17 @@
 
 # Update
 sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt-get -y upgrade 
 sudo apt-get -y dist-upgrade
 
 # Install gui
-sudo apt-get install --no-install-recommends xserver-xorg
-sudo apt-get install --no-install-recommends raspberrypi-ui-mods lxterminal gvfs
-sudo apt-get install chromium-browser
+sudo apt-get install --no-install-recommends xserver-xorg -y
+sudo apt-get install --no-install-recommends xinit -y
+sudo apt-get install raspberrypi-ui-mods -y
+sudo apt-get install --no-install-recommends rpi-chromium-mods -y
+sudo apt-get install xrdp -y
 
 # Clean out cache
 sudo apt-get clean
 
+sudo shutdown -r now
