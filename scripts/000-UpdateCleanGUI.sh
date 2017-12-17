8f3ce0a3b4b4865bd69471ce81365dd7bb254965
Bertus Kruger <krugercreations@gmail.com>
2017-12-17 13:51:52 +1300

    Update 000-UpdateCleanGUI.sh

--- scripts/000-UpdateCleanGUI.sh
+++ scripts/000-UpdateCleanGUI.sh
@@ -6,12 +6,15 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
 # Install gui
-sudo apt-get install --no-install-recommends xserver-xorg -y
-sudo apt-get install --no-install-recommends xinit -y
-sudo apt-get install raspberrypi-ui-mods -y
-sudo apt-get install --no-install-recommends rpi-chromium-mods -y
-sudo apt-get install xrdp -y
-sudo apt-get install git -y
+#sudo apt-get install --no-install-recommends xserver-xorg -y
+#sudo apt-get install --no-install-recommends xinit -y
+#sudo apt-get install raspberrypi-ui-mods -y
+#sudo apt-get install --no-install-recommends rpi-chromium-mods -y
+#sudo apt-get install xrdp -y
+#sudo apt-get install git -y
+
+sudo apt-get -y remove --purge libreoffice-*
+sudo apt-get -y remove --purge wolfram-engine
 
 # Clean out cache
 sudo apt-get clean
