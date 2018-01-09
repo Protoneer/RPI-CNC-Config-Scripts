65f48f546b42b553024f7c60997318e0a1d0119e
Bertus Kruger <krugercreations@gmail.com>
2018-01-09 20:19:11 +1300

    Update 006-install-laserweb.sh

--- scripts/006-install-laserweb.sh
+++ scripts/006-install-laserweb.sh
@@ -4,16 +4,4 @@ cd lw.comm-server
 sudo npm install serialport --unsafe-perm --build-from-source
 sudo npm install
 
-#cd ~
-#git clone https://github.com/openhardwarecoza/LaserWeb3.git
-#cd LaserWeb3
-#npm install
-
-
-# Old
-#sudo apt-get -y install build-essential python g++ make
-#cd ~
-#sudo git clone https://github.com/openhardwarecoza/LaserWeb.git
-#cd LaserWeb
-#sudo npm install -g serialport@1.6.1
-#sudo npm install -g socket.io node-static
+echo "RESET_ON_CONNECT=1" >> .env
