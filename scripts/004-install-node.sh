589cdde18026f3e99a665aa505fe445476845158
Bertus Kruger <krugercreations@gmail.com>
2019-10-10 09:07:46 +1300

    Update 004-install-node.sh

--- scripts/004-install-node.sh
+++ scripts/004-install-node.sh
@@ -20,5 +20,10 @@ sudo apt-get -y remove nodejs
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 
 #sudo shutdown -r now
