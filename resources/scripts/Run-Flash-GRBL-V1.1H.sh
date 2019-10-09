d96dacf9207ca49e275f34b2e16f5ee23cee7a0e
Bertus Kruger <krugercreations@gmail.com>
2019-10-10 10:30:41 +1300

    Create Run-Flash-GRBL-V1.1H.sh

---
+++ resources/scripts/Run-Flash-GRBL-V1.1H.sh
@@ -0,0 +1,1 @@
+/home/pi/arduino-1.8.5/hardware/tools/avr/bin/avrdude -C/home/pi/arduino-1.8.5/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyAMA0 -b115200 -D -Uflash:w:/home/pi/RPI-CNC-Config-Scripts/resources/firmware/grbl_v1.1h.20190825.hex:i
