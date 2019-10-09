e1e244a54368515b9f36b5e5c19eddd4e2d3a0ce
Bertus Kruger <krugercreations@gmail.com>
2019-10-10 08:46:27 +1300

    Update 004-install-node.sh

--- scripts/004-install-node.sh
+++ scripts/004-install-node.sh
@@ -6,16 +6,19 @@ sudo apt-get -y remove nodejs
 
 
 #RPI 3
-wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv7l.tar.xz
-tar -xvf node-v8.11.1-linux-armv7l.tar.xz
-cd node-v8.11.1-linux-armv7l
-sudo cp -R * /usr/local/
+#wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv7l.tar.xz
+#tar -xvf node-v8.11.1-linux-armv7l.tar.xz
+#cd node-v8.11.1-linux-armv7l
+#sudo cp -R * /usr/local/
 
 
 # RPI 1+2
-wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv6l.tar.xz
-tar -xvf node-v8.11.1-linux-armv6l.tar.xz
-cd node-v8.11.1-linux-armv6l
-sudo cp -R * /usr/local/
+#wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv6l.tar.xz
+#tar -xvf node-v8.11.1-linux-armv6l.tar.xz
+#cd node-v8.11.1-linux-armv6l
+#sudo cp -R * /usr/local/
 
-sudo shutdown -r now
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#sudo shutdown -r now
