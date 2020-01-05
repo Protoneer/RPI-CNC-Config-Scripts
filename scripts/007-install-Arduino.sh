4be4fdb66a1bb895089aff9e0919389bad32b5f7
Bertus Kruger <krugercreations@gmail.com>
2020-01-05 15:23:09 +1300

    Update 007-install-Arduino.sh
    Removed version number from arduino folder

--- scripts/007-install-Arduino.sh
+++ scripts/007-install-Arduino.sh
@@ -5,21 +5,20 @@ sudo apt-get install -y python-dev python-rpi.gpio
 # install Aruino
 wget https://downloads.arduino.cc/arduino-1.8.10-linuxarm.tar.xz -P ~
 tar xf ~/arduino-1.8.10-linuxarm.tar.xz -C ~
+tar xf ~/arduino-1.8.10-linuxarm.tar.xz -C ~/arduino
 rm ~/arduino-1.8.10-linuxarm.tar.xz
 
 # Install avrdude-rpi
-cp avrdude-rpi/autoreset2560 ~/arduino-1.8.10/hardware/tools/avr/bin/
-cp avrdude-rpi/autoreset328 ~/arduino-1.8.10/hardware/tools/avr/bin/
-cp avrdude-rpi/avrdude-autoreset ~/arduino-1.8.10/hardware/tools/avr/bin/
-sudo mv ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude-original
-sudo ln -s ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude-autoreset ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude
+cp avrdude-rpi/autoreset2560 ~/arduino/hardware/tools/avr/bin/
+cp avrdude-rpi/autoreset328 ~/arduino/hardware/tools/avr/bin/
+cp avrdude-rpi/avrdude-autoreset ~/arduino/hardware/tools/avr/bin/
+sudo mv ~/arduino/hardware/tools/avr/bin/avrdude ~/arduino/hardware/tools/avr/bin/avrdude-original
+sudo ln -s ~/arduino/hardware/tools/avr/bin/avrdude-autoreset ~/arduino/hardware/tools/avr/bin/avrdude
 
 
-sudo chmod +x ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude
-sudo chmod +x ~/arduino-1.8.10/hardware/tools/avr/bin/autoreset328
-sudo chmod +x ~/arduino-1.8.10/hardware/tools/avr/bin/autoreset2560
+sudo chmod +x ~/arduino/hardware/tools/avr/bin/avrdude
+sudo chmod +x ~/arduino/hardware/tools/avr/bin/autoreset328
+sudo chmod +x ~/arduino/hardware/tools/avr/bin/autoreset2560
 
 # Make AVRDUDE available for all
-sudo ln -s -T ~/arduino-1.8.10/hardware/tools/avr/bin/avrdude /usr/local/bin/avrdude
-#  export PATH=$PATH:~/arduino-1.8.10/hardware/tools/avr/bin
-
+sudo ln -s -T ~/arduino/hardware/tools/avr/bin/avrdude /usr/local/bin/avrdude
