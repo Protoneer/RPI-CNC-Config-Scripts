bd83da948ec8190e0ba089383753ca044101817e
Bertus Kruger <krugercreations@gmail.com>
2020-01-05 15:11:02 +1300

    Update 004-install-node.sh

--- scripts/004-install-node.sh
+++ scripts/004-install-node.sh
@@ -1,14 +1,9 @@
 sudo apt-get -y remove nodejs
 
-# RPi 3
-#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-
-
 #RPI 3-4
-wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv7l.tar.xz
-tar -xvf node-v8.11.1-linux-armv7l.tar.xz
-cd node-v8.11.1-linux-armv7l
+wget https://nodejs.org/dist/v12.14.0/node-v12.14.0-linux-armv7l.tar.xz
+tar -xvf node-v12.14.0-linux-armv7l.tar.xz
+cd node-v12.14.0-linux-armv7l.tar.xz
 sudo cp -R * /usr/local/
 
 
@@ -17,13 +12,3 @@ wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv6l.tar.xz
 tar -xvf node-v8.11.1-linux-armv6l.tar.xz
 cd node-v8.11.1-linux-armv6l
 sudo cp -R * /usr/local/
-
-#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-#sudo apt-get install gcc g++ make
-#curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-#sudo apt-get update && sudo apt-get install yarn
-
-
-#sudo shutdown -r now
