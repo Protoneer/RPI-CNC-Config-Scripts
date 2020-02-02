faa6f2d7534fab76a3be9384d3a81929206e877a
Bertus Kruger <krugercreations@gmail.com>
2020-02-02 20:20:19 +1300

    Update 004-install-node.sh

--- scripts/004-install-node.sh
+++ scripts/004-install-node.sh
@@ -1,7 +1,7 @@
-cd ~
 sudo apt-get -y remove nodejs
 
 #RPI 3-4
+cd ~
 wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv7l.tar.xz
 tar -xvf node-v8.11.1-linux-armv7l.tar.xz
 cd node-v8.11.1-linux-armv7l
@@ -9,7 +9,12 @@ sudo cp -R * /usr/local/
 
 
 # RPI 1+2
+cd ~
 wget https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-armv6l.tar.xz
 tar -xvf node-v8.11.1-linux-armv6l.tar.xz
 cd node-v8.11.1-linux-armv6l
 sudo cp -R * /usr/local/
+
+cd ~
+rm node-v8.11.1-linux-armv6l.tar.xz
+rm node-v8.11.1-linux-armv7l.tar.xz
